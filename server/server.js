9bffef454f7575d832fce759165367f7d617a8fd
graygabrielle <gray.atkinson.adams@gmail.com>
2019-04-15 14:05:11 -0700

    starting survey

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const db = require("../db/models");
-const router = require("./routes/index");
+const router = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 
