b1d44b81f4b39d9f4c6e5a0bd702d7b9d8a2c53c
graygabrielle <gray.atkinson.adams@gmail.com>
2019-04-21 14:54:47 -0700

    finished styling survey

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = require("./routes");
-const db = require("../db/models");
 
 const PORT = process.env.PORT || 3000;
 
